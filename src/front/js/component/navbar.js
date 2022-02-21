017074759e072630de757210a53831d7dbb52d38
Carolina Araya <ci.arayagonzalez@gmail.com>
2022-02-21 18:26:57 +0000

    featNavbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import sw  from "../../img/logo.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar-light bg-dark">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-3">
+            <Link to="/">
+			<img className="w-50" src={sw} />
+            </Link>
+          </div>
+		  <div className="col-6 d-flex justify-content-center my-auto">
+			  <span className="text-light mx-3">CHARACTERS</span>
+			  <span className="text-light mx-3">PLANETS</span>
+			  <span className="text-light mx-3">STARSHIPS</span>
+		  </div>
+
+          <div className="col-3 d-flex justify-content-center my-auto">
+            <Link className="mx-2" to="/signup">
+              <button className="btn btn-outline-warning">Sign Up</button>
+            </Link>
+			<Link className="mx-2" to="/login">
+              <button className="btn btn-outline-warning">Log In</button>
+            </Link>
+          </div>
+         
+        </div>
+      </div>
+    </nav>
+  );
 };
