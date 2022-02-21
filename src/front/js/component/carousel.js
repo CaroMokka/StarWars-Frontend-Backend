f363f2bdb90b8d4d7a288714d47081432e7a550b
Carolina Araya <ci.arayagonzalez@gmail.com>
2022-02-21 21:39:09 +0000

    featCarousel

---
+++ src/front/js/component/carousel.js
@@ -0,0 +1,97 @@
+import React from "react";
+
+export const Carousel = () => {
+  return (
+    <div
+      id="carouselExampleCaptions"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="https://starwarsblog.starwars.com/wp-content/uploads/2022/01/star-wars-the-high-republic-13-cover_TALL-1536x512.jpg" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h2>Avar Kriss Dances with the Darkness in Marvels Star Wars: The High Republic #13  Exclusive Preview </h2>
+            <p>As Aphra is about to discover, its more technological marvel than ritualistic magic.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://starwarsblog.starwars.com/wp-content/uploads/2022/01/the-book-of-boba-fett-ch-5-cargo-hold-TALL-1-1536x512.jpg" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h2>The Book of Boba Fett Cargo Hold: “Chapter 5: Return of the Mandalorian”</h2>
+            <p>Check out concept art, stills, and more from the fifth episode of the series.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://starwarsblog.starwars.com/wp-content/uploads/2022/01/lego-starwars-sws-group-tall-8974-1536x512.jpg" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h2>New LEGO Star Wars: The Skywalker Saga Trailer and Launch Date Revealed</h2>
+            <p>The biggest look yet at the biggest LEGO Star Wars game ever, coming this April.</p>
+          </div>
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+};
+
+{
+  /* <div className="container">
+      <div
+        id="carouselExampleSlidesOnly"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="https://www.xtrafondos.com/wallpapers/resized/star-wars-el-despertar-de-la-fuerza-poster-3650.jpg?s=large" className="d-block w-75" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="https://www.xtrafondos.com/wallpapers/resized/stormtrooper-de-star-wars-battlefront-5226.jpg?s=large" className="d-block w-75" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="https://www.xtrafondos.com/wallpapers/resized/todos-los-personajes-de-star-wars-2307.jpg?s=large" className="d-block w-75" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div> */
+}
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,27 +1,24 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+
 import "../../styles/home.css";
+import { Carousel } from "../component/carousel";
 
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="container mt-5">
+
+			<Carousel/>
+
+			<hr className="border border-1"/>
+			
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+		
 		</div>
 	);
 };
